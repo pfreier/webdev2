aa158de09c45da61c5b8bb79f210f5c71f9d6e88
pfreier <patrick_f12@gmx.de>
2015-05-01 17:54:56 +0200

    created model classes and migration files for: Dozent_Spamlist Email

--- WebDev2/app/Email.php
+++ WebDev2/app/Email.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model {
 
-	//
+	//Email des Studenten
+
+	public function spamlist(){
+		return $this->belongsTo('App/Dozent_Spamlist');
+	}
 
 }
