f3357420c236e9f1fbf3f8ff1000e68d5383cee6
felixwalter <student@ubuntu>
2015-05-06 14:49:32 +0200

    model, migration

---
+++ WebDev2/database/migrations/2015_05_04_202830_create_dozents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDozentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('dozents', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('vorname');
+			$table->string('nachname');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('dozents');
+	}
+
+}
